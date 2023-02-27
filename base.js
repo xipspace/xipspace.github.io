ef0f7967ffc5de5cc3fc9f86ed163eda0e9b65e4
xipspace <helloxipi@gmail.com>
2023-02-27 15:25:07 -0300

    Setup content for cards

--- base.js
+++ base.js
@@ -1,28 +1,43 @@
 $(document).ready(function () {
 
-  $(".item").click(function () {
+	$(".item").click(function () {
 
-    // $(this).toggleClass("selected");
-    var str = $(this).text();
-    // $("." + str).toggle();
-    switch (str) {
-      case "main":
-        $(".main").show();
-        $(".showcase").hide();
-        $(".design").hide();
-        break;
-      case "showcase":
-        $(".main").hide();
-        $(".showcase").show();
-        $(".design").hide();
-        break;
-      case "design":
-        $(".main").hide();
-        $(".showcase").hide();
-        $(".design").show();
-        break;
-    }
+		// $(this).toggleClass("selected");
+		var str = $(this).text();
+		// $("." + str).toggle();
+		switch (str) {
+			case "main":
+				$(".main").show();
+				$(".showcase").hide();
+				$(".design").hide();
+				break;
+			case "showcase":
+				$(".main").hide();
+				$(".showcase").show();
+				$(".design").hide();
+				break;
+			case "design":
+				$(".main").hide();
+				$(".showcase").hide();
+				$(".design").show();
+				break;
+		}
 
-  });
+	});
+
+	$("button").click(function () {
+
+		var contentLoad = "https://raw.githubusercontent.com/xipspace/xipspace.github.io/main/README.md";
+
+		$(".main").load(contentLoad, function (responseTxt, statusTxt, xhr) {
+
+			if (statusTxt == "success")
+				console.debug("External content loaded successfully!");
+			if (statusTxt == "error")
+				console.debug("Error: " + xhr.status + ": " + xhr.statusText);
+
+		});
+
+	});
 
 });
