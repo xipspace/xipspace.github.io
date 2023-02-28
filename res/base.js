918d5c1f8707953d7736038a4865e6b6d606b007
xipspace <helloxipi@gmail.com>
2023-02-28 04:07:00 -0300

    Inserts card content

--- res/base.js
+++ res/base.js
@@ -9,31 +9,8 @@ function cardsCreator(x){
 
 $(document).ready(function () {
 
-/*
-	$(".cards").load(content, function (responseTxt, statusTxt, xhr) {
-
-		if (statusTxt == "success")
-			console.debug("External content loaded successfully : " +
-				xhr.getResponseHeader("content-type"))
-		if (statusTxt == "error")
-			console.debug("Error: " + xhr.status + ": " + xhr.statusText)
-
-	})
-*/
-
-	var jsonObj = $.getJSON(content)
-		.done(function () {
-			console.log("success")
-		})
-		.fail(function () {
-			console.log("error")
-		})
-		.always(function () {
-			console.log("complete")
-		})
-	
-	jsonObj.always(function(){
-		cardsCreator(jsonObj.responseText)
+	$(".item").each(function(index){
+		console.log(index + ": " + $(this).text())
 	})
 
 	$(".item").click(function () {
@@ -59,6 +36,36 @@ $(document).ready(function () {
 				break
 		}
 
+	})
+
+	/*
+	$(".cards").load(content, function (responseTxt, statusTxt, xhr) {
+
+		if (statusTxt == "success")
+			console.debug("External content loaded successfully : " +
+				xhr.getResponseHeader("content-type"))
+		if (statusTxt == "error")
+			console.debug("Error: " + xhr.status + ": " + xhr.statusText)
+
+	})
+	*/
+
+	var jsonObj = $.getJSON(content)
+		.done(function () {
+			console.log("success")
+		})
+		.fail(function () {
+			console.log("error")
+		})
+		.always(function () {
+			console.log("complete")
+		})
+
+	jsonObj.always(function (data) {
+		console.log(data)
+		// $(".cover").attr("src", data.cards.cover)
+		$(".title").text(data.cards.title)
+		$(".description").text(data.cards.description)
 	})
 
 })
