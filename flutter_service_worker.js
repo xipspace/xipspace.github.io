a55288a885d36429622e152188b5398f1ac01c80
xipspace <helloxipi@gmail.com>
2024-02-04 00:07:41 -0300

    implement minimal index search function

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -17,9 +17,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "8b641c4c34b9feded4914ff7b0f6321f",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "5fa5443281544e986f4ff02928615477",
-"/": "5fa5443281544e986f4ff02928615477",
-"main.dart.js": "c31af93b2ee5adb3f16bcbf1f2ce8db6",
+"index.html": "05479d0dc7c2c59dd0c591d7bf33cf7e",
+"/": "05479d0dc7c2c59dd0c591d7bf33cf7e",
+"main.dart.js": "0ef9fae745494464b5fe27fa85697277",
 "manifest.json": "b5236c42ab0b97a35f65e7e8cebd17fb",
 "version.json": "a3c75d1ef0f82cbcf163ce48c16eaf48"
 };
