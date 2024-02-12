fbf6df85f3d0e36301aa4f38dc4f130cacc6fe10
xipspace <helloxipi@gmail.com>
2024-02-11 22:35:18 -0300

    update index tracker to store on user interaction

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -20,9 +20,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "8b641c4c34b9feded4914ff7b0f6321f",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "6a0d9255d476332ece331fba54afc995",
-"/": "6a0d9255d476332ece331fba54afc995",
-"main.dart.js": "7bbe12b12c16ff39f562e023604f38fe",
+"index.html": "b1e040b33f077dced94295644ade957a",
+"/": "b1e040b33f077dced94295644ade957a",
+"main.dart.js": "56aea296d0fe8cdbf3916161770fe2d8",
 "manifest.json": "b5236c42ab0b97a35f65e7e8cebd17fb",
 "version.json": "a3c75d1ef0f82cbcf163ce48c16eaf48"
 };
