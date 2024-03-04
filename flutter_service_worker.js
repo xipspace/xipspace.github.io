4bbb86f71c4ced21c2ce75e6525f1fc2105d10d4
xipspace <helloxipi@gmail.com>
2024-03-04 00:51:38 -0300

    will reset the grid with a new game

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -23,9 +23,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "8b641c4c34b9feded4914ff7b0f6321f",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "cb40c538df031810b6546f805cb44df7",
-"/": "cb40c538df031810b6546f805cb44df7",
-"main.dart.js": "b2e00a48253d0cc12235b877f522d8dd",
+"index.html": "c3967a19c78a27ce10cfaa2ee87c353f",
+"/": "c3967a19c78a27ce10cfaa2ee87c353f",
+"main.dart.js": "d5444c53dfbd33f357f23c001ee16157",
 "manifest.json": "b5236c42ab0b97a35f65e7e8cebd17fb",
 "version.json": "a3c75d1ef0f82cbcf163ce48c16eaf48"
 };
