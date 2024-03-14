1b7b761104a1f0f25e64d1b09056cb2a0bc67452
xipspace <helloxipi@gmail.com>
2024-03-14 02:10:26 -0300

    basic workflow milestone

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -23,9 +23,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "8b641c4c34b9feded4914ff7b0f6321f",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "f6f1e5c707603749885f7bfd02a29939",
-"/": "f6f1e5c707603749885f7bfd02a29939",
-"main.dart.js": "d93b4f3da7436994956f663efecf4c52",
+"index.html": "14c903f40e5b4baf5fd442c7667b97ff",
+"/": "14c903f40e5b4baf5fd442c7667b97ff",
+"main.dart.js": "e31ee8df33e0c9e3d756f7d86ffb9241",
 "manifest.json": "b5236c42ab0b97a35f65e7e8cebd17fb",
 "version.json": "a3c75d1ef0f82cbcf163ce48c16eaf48"
 };
