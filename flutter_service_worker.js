5d53ff6467be23ecb3ef1597ca8fd6a8d35cd2a2
xipspace <helloxipi@gmail.com>
2024-03-26 00:59:49 -0300

    back with static theme

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -24,9 +24,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "8b641c4c34b9feded4914ff7b0f6321f",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "7e49cb1853c8da672aae4dec8e279b30",
-"/": "7e49cb1853c8da672aae4dec8e279b30",
-"main.dart.js": "28d720eb61e070ff9a5eacb8e11516df",
+"index.html": "44213e14f498efaa4c6392f050b3fc7d",
+"/": "44213e14f498efaa4c6392f050b3fc7d",
+"main.dart.js": "675c383dce2deb5f5fe7002ca8d38e64",
 "manifest.json": "b5236c42ab0b97a35f65e7e8cebd17fb",
 "version.json": "a3c75d1ef0f82cbcf163ce48c16eaf48"
 };
